8980c63e3386f997d68c8ae2a0a69a6b52eac3b5
Mike <19901759+wakandavibranium@users.noreply.github.com>
2025-06-25 14:56:28 +0300

    feat(scripts): add a script for creating tenants on RDS

--- scripts/server_setup.sh
+++ scripts/server_setup.sh
@@ -24,5 +24,31 @@ check_and_install_zip() {
     fi    
 }
 
+check_and_install_curl() {
+    # Check if curl is installed
+    if ! command -v curl &> /dev/null
+    then
+        echo "Curl not found. Installing curl..."
+        sudo apt-get update
+        sudo apt-get install -y curl
+    else
+        echo "Curl is already installed."
+    fi
+}
+
+check_and_install_postgresql() {
+    # Check if PostgreSQL is installed
+    if ! command -v psql &> /dev/null
+    then
+        echo "PostgreSQL not found. Installing PostgreSQL..."
+        sudo apt-get update
+        sudo apt-get install -y postgresql postgresql-contrib
+    else
+        echo "PostgreSQL is already installed."
+    fi
+}
+
 check_and_install_net_tools
 check_and_install_zip
+check_and_install_curl
+check_and_install_postgresql
